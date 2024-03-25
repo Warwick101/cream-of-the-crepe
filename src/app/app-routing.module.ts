78d5ddd8d1fe27945eb30e3d1e49992001eef90f
Warwick de Kock <war.wick101@gmail.com>
2024-03-25 16:23:36 -0500

    adds typo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {MenuComponent} from "./menu/menu.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'menu', pathMatch: 'full'},
+  {path: 'menu', component: MenuComponent, title: 'Menu'},
+  {path: '**', component: HomeComponent, title: 'Home'},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
