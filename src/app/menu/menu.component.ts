afad1c81772a6b1b0af4030519c420e61013bdc5
Warwick de Kock <war.wick101@gmail.com>
2024-04-05 20:34:43 -0500

    update data

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -13,21 +13,21 @@ export class MenuComponent implements OnInit {
   menuData = [
     {
       category: "Savory Crêpes",
+      img:'../../assets/img/food-item.png',
       type: "savory",
       caption: "Add an egg to any menu item $.75",
       items: [
         { title: "Rise and Shine", description: "Eggs, turkey bacon & your choice of cheese (mozzarella, swiss or cheddar)",
-          price: 12.95,
-          img:'../../assets/img/food-item.png'
+          price: 12.95
         },
         { title: "Greekfast", description: "Eggs, feta cheese, spinach & tomatoes", price: 11.95 },
         { title: "Halima Special", description: "Egg blended mozzarella, swiss & feta cheese", price: 11.5 },
         { title: "Breakfast Burrito", description: "Your choice turkey or sliced roast beef with onions, green peppers, egg & cheddar", price: 13.5 },
-        // Add more items as needed
       ]
     },
     {
       category: "Sweet Crêpes",
+      img:'../../assets/img/food-item.png',
       type: "sweet",
       caption: "Add one scoop of ice cream for $x.x",
       items: [
@@ -37,11 +37,11 @@ export class MenuComponent implements OnInit {
         },
         { title: "Totally Nutty", description: "Peanut butter with Nutella spread or fruit preserves", price: 10.5 },
         { title: "Crazy for Chocolate", description: "Chocolate crepe, chocolate sauce, chocolate rice krispies & raspberry sauce", price: 10.5 },
-        // Add more items as needed
       ]
     },
     {
       category: "French Takos",
+      img:'../../assets/img/food-item.png',
       type: "savory",
       items: [
         { title: "Paris Original Takos", description: "Marinated chicken, fries, caramelized onion homemade french cheese sauce", price: 13.5 },
@@ -49,20 +49,29 @@ export class MenuComponent implements OnInit {
         { title: "Marrakeeh Takos", description: "Ground beef kafta, fries, cheddar cheese, homemade french cheese sauce", price: 13.5,
           img:'../../assets/img/food-item.png'
         },
-        // Add more items as needed
+      ]
+    },
+    {
+      category: "French Takos",
+      img:'../../assets/img/food-item.png',
+      type: "savory",
+      items: [
+        { title: "Paris Original Takos", description: "Marinated chicken, fries, caramelized onion homemade french cheese sauce", price: 13.5 },
+        { title: "Cordon Blue Takos", description: "Crispy chicken tenders swiss cheese, fries,homemade french cheese sauce", price: 13.5 },
+        { title: "Marrakeeh Takos", description: "Ground beef kafta, fries, cheddar cheese, homemade french cheese sauce", price: 13.5,
+          img:'../../assets/img/food-item.png'
+        },
       ]
     },
     {
       category: "Drinks",
+      img:'../../assets/img/food-item.png',
       type: "sweet",
       items: [
         { title: "Coffee (Regular or decaf)", description: "", price: 3.25 },
-        { title: "Espresso", description: "", price: 3.25,
-          img:'../../assets/img/food-item-sweet.png'
+        { title: "Espresso", description: "", price: 3.25
         },
-        { title: "Americano", description: "", price: 3.5,
-          img:'../../assets/img/food-item.png'},
-        // Add more items as needed
+        { title: "Americano", description: "", price: 3.5}
       ]
     },
     {
@@ -71,13 +80,11 @@ export class MenuComponent implements OnInit {
       items: [
         { title: "Croissant", description: "", price: 3.25 },
         { title: "Chocolate Croissant", description: "", price: 3.95 },
-        { title: "Amand Croissant", description: "", price: 4.5,
-          img:'../../assets/img/food-item.png'},
-        // Add more items as needed
+        { title: "Amand Croissant", description: "", price: 4.5}
       ]
     },
     {
-      category: "Gelatop",
+      category: "Gelato",
       type: "sweet",
       items: [
         { title: "One Scoop", description: "Mixed, Berry, Vanilla, Chocolate, Coconut Almonds..", price: 3,
