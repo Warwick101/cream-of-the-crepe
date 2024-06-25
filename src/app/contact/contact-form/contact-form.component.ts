a5c5b4665cfa488487d3d54f453a80517f95b66f
Warwick de Kock <war.wick101@gmail.com>
2024-06-25 12:58:32 -0500

    adds banner

--- src/app/contact/contact-form/contact-form.component.ts
+++ src/app/contact/contact-form/contact-form.component.ts
@@ -2,6 +2,12 @@ import {Component, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, FormGroupDirective, Validators} from '@angular/forms';
 import {Subscription} from 'rxjs';
 import {DatePipe} from '@angular/common';
+import {
+  WdkBannerComponentData,
+  WdkBannerService,
+  WdkBannerType
+} from "@wjdk/acl";
+
 
 @Component({
   selector: 'app-form',
@@ -15,17 +21,16 @@ export class ContactFormComponent implements OnInit, OnDestroy {
   @Input() phoneValue: string = '';
   // @ViewChild(FormGroupDirective, { static: true }) formGroupDirective: FormGroupDirective;
 
+  error = WdkBannerType.Error;
+  info = WdkBannerType.Info;
+  success = WdkBannerType.Success;
+  warning = WdkBannerType.Warning;
+  data: WdkBannerComponentData;
 
   contactForm: FormGroup;
-  // form: FormGroup;
-  alertMessage = 'default';
-  alertIcon = 'default';
-  // sendMessage = false;
-  messageTrigger = false;
-  // statusSubscription: Subscription;
   datePipeEn: DatePipe = new DatePipe('en-US');
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private bannerService: WdkBannerService ) {
 
     this.contactForm = this.fb.group({
       name: new FormControl(''),
@@ -33,81 +38,39 @@ export class ContactFormComponent implements OnInit, OnDestroy {
       phone: new FormControl(''),
       message: new FormControl(''),
     })
-    // this.contactForm = new FormGroup({
-    //   name: new FormControl(''),
-    //   email: new FormControl(''),
-    //   message: new FormControl(''),
-    // })
   }
 
   ngOnInit() {
-    this.initForm();
+
   }
 
-  initForm() {
-    // this.queenForm = new FormGroup({
-    //   name: new FormControl(null, Validators.required),
-    //   surname: new FormControl(null, [Validators.required]),
-    //   phone: new FormControl(null, Validators.maxLength(20)),
-    //   email: new FormControl(null, [Validators.required]),
-    //   patientMessage: new FormControl(null, [Validators.required]),
-    // });
-  }
 
   onSubmit() {
     const emailData: any = {
-      // name: this.contactForm['name'].value,
-      // email: this.contactForm['email'].value,
-      // message: this.contactForm['message'].value,
-      // emailDate: new Date(),
-      // messageTitle: `New message from ${this.queenForm.get('name').value + ' ' +  this.queenForm.get('surname').value}`,
-      // adminStatus: 'website',
-      // messageType: 'contact',
-      // messageSubject: 'Website Message',
-      // status: 'siteSent',
-      // sendTo: 'war.wick101@gmail.com',
-      // patient: null,
     };
     console.log(this.contactForm);
     console.log(emailData);
     // this.messageAnimation();
+    this.sendBanner(this.success)
   }
 
-  // Refactored this is as a component
-
-  // messageAnimation() {
-  //   this.sendMessage = true;
-  //     setTimeout(() => {
-  //       this.sendMessage = false;
-  //     }, 4000);
-  // }
-
-  // Initially used states that changed programmatically
-  //
-  // messageAnimation() {
-  //   this.alertMessage = 'submitted';
-  //
-  //   setTimeout(() => {
-  //     this.alertIcon = 'submitted';
-  //   }, 300);
-  //
-  //   setTimeout(() => {
-  //     this.alertMessage = 'submittedEnd';
-  //   }, 3000);
-  // }
-  //
-  // getMessageStyle(): string {
-  //   if (this.alertMessage == 'submitted') {
-  //     return 'alerted-message'
-  //   }
-  //   else {
-  //     return '';
-  //   }
-  // }
-
-  isInvalidEmail = () => {
-    // return this.queenForm.errors && this.queenForm.errors.isInvalidEmail;
+  sendBanner(alert: WdkBannerType) {
+    this.data = {
+      bannerType: alert,
+      bodyDetails: 'This is some additional information for new portal banner.',
+      closeCallBack: this.close,
+      headline: 'This is custom headline',
+      body: 'This is custom body',
+      outletId: '#banner',
+    };
+    this.bannerService.sendAlert(this.data);
   }
+
+  close = () => {
+    // provide product specific cleanup code and then call BannerService close method
+    this.bannerService.close();
+  }
+
 
   ngOnDestroy(): void {
     // this.statusSubscription.unsubscribe();
